44e794585968994952de42ac7d08b108ff118ff8
calvin <calvinchenhupro@gmail.com>
2021-03-01 01:35:02 -0800

    updated

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ $(document).ready(function(){
   $app.html('');
   // renderFeed();
 
+
   // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
   var $button = $('<button>Update Feed</button>');
-  // var $tweets = $('<div id="tweets"></div>');
+  var $tweets = $('<div></div>');
 
 
   // Create event handler functions
@@ -22,10 +23,12 @@ $(document).ready(function(){
   $title.on("click", handleTitleClick);
   $button.on("click", handleTitleClick);
 
+
   // Appdend new HTML elements to the DOM
   $title.appendTo($app);
   $button.appendTo($app);
-  // $tweets.appendTo($app);
+  $tweets.appendTo($app);
+
 
   // var renderFeed = function() {
   //   // clear and populate the #feed
@@ -46,11 +49,25 @@ $(document).ready(function(){
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet-box"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
+    var $tweet = $('<div class="container"></div>');
+    var $image = $('<img class="profile-photo", src="assets/img/maracus.png"></img>');
+    var $user = $('<span class="username"></span>');
+    var $message = $('<div class="message"></div>');
+    var $time =  $('<span class="timestamp"></span>');
+    var $icon1 = $('<img class="icon", src="assets/icons/placeholder.png"></img>');
+    var $icon2 = $('<img class="icon", src="assets/icons/placeholder.png"></img>');
+    var $icon3 = $('<img class="icon", src="assets/icons/placeholder.png"></img>');
+    var $icon4 = $('<img class="icon", src="assets/icons/placeholder.png"></img>');
+    // $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $user.text("@" + tweet.user);
+    $message.text(tweet.message);
+    $time.append(tweet.created_at);
+    $tweet.append($image, $user, $message, $time, $icon1, $icon2, $icon3, $icon4);
+    $tweet.appendTo($tweets);
     index -= 1;
+
   }
+
 
   // var updateFeed = function() {
   //   // clear and populate the #feed
