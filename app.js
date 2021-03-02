1c25591296e71f7014f248e2083f1b6c62b5cf2b
calvin <calvinchenhupro@gmail.com>
2021-03-01 18:25:42 -0800

    Assignment complete

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
 
   jQuery("time.timeago").timeago();
 
+
   // Select already exsiting elements
   var $app = $('#app');
   // console.log('tweets container', $app)
   $app.html('');
-  // renderFeed();
 
 
   // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
-  var $button = $('<button>Update Feed</button>');
+  var $button = $('<button id="button">Update Feed</button>');
   var $tweets = $('<div id="tweets"></div>');
+
+
+  // Appdend new HTML elements to the DOM
+  $title.appendTo($app);
+  $button.appendTo($app);
+  $tweets.appendTo($app);
+  updateFeed();
 
 
   // Create event handler functions
@@ -20,21 +27,26 @@ $(document).ready(function(){
     alert('The title of this page is: ' + event.target.innerText);
   }
 
-  var updateFeed = function() {
+  var handleUsernameClick = function(event) {
+    document.querySelector('#button').innerHTML = 'Back';
+  }
 
 
-    console.log('running updateFeed')
+  function updateFeed() {
+
+    // console.log('running updateFeed')
     // clear and populate the #feed
     var $tweets = $('#tweets');
-    console.log('tweets container', $tweets)
+    // console.log('tweets container', $tweets)
     $tweets.html("");
-
 
     var index = streams.home.length - 1;
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div class="container"></div>');
+
       var $image = $('<img class="profile-photo", src="assets/img/mracus.png"></img>');
+
       var $user = $('<span class="username"></span>');
       var $message = $('<div class="message"></div>');
       var $time = $('<span class="timestamp"></span>');
@@ -45,6 +57,50 @@ $(document).ready(function(){
       // $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $user.text("@" + tweet.user);
       $message.text(tweet.message);
+      $time.append(jQuery.timeago(new Date()));
+
+      $icon1.append('<i class="fas fa-bowling-ball"></i>');
+      $icon2.append('<i class="fas fa-bowling-ball"></i>');
+      $icon3.append('<i class="fas fa-bowling-ball"></i>');
+      $icon4.append('<i class="fas fa-bowling-ball"></i>');
+      $tweet.append($image, $user, $message, $time, $icon1, $icon2, $icon3, $icon4);
+      $tweet.appendTo($tweets);
+      index -= 1;
+
+      var username = tweet.user;
+      $user.on("click", function() {
+        renderUserTweets(username);
+      });
+
+    }
+
+  }
+
+  function renderUserTweets(name) {
+    var $tweets = $('#tweets');
+    // console.log('tweets container', $tweets)
+    $tweets.html("");
+    var $name = $("<div class='username'></div>");
+    $name.append("@" + name);
+    $tweets.append($name);
+
+    var index = streams.users[name].length - 1;
+    while(index >= 0){
+      var userTweet = streams.users[name][index];
+
+      var $tweet = $('<div class="container"></div>');
+      var $image = $('<img class="profile-photo", src="assets/img/mracus.png"></img>');
+      var $user = $('<span class="username"></span>');
+      var $message = $('<div class="message"></div>');
+      var $time = $('<span class="timestamp"></span>');
+      var $icon1 = $('<span class="icon"></span>');
+      var $icon2 = $('<span class="icon"></span>');
+      var $icon3 = $('<span class="icon"></span>');
+      var $icon4 = $('<span class="icon"></span>');
+      // $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $user.text("@" + userTweet.user);
+      $message.text(userTweet.message);
+
       $time.append(jQuery.timeago(new Date()));
       $icon1.append('<i class="fas fa-bowling-ball"></i>');
       $icon2.append('<i class="fas fa-bowling-ball"></i>');
@@ -61,46 +117,41 @@ $(document).ready(function(){
 
   // Set event listeners (providing appropriate handlers as input)
   $title.on("click", handleTitleClick);
-  $button.on("click", updateFeed);
+  $button.on("click", function todo() {
+    handleUsernameClick(); updateFeed();
+  });
 
 
-  // Appdend new HTML elements to the DOM
-  $title.appendTo($app);
-  $button.appendTo($app);
-  $tweets.appendTo($app);
+  // var index = streams.home.length - 1;
+  // while(index >= 0){
+  //   var tweet = streams.home[index];
+  //   var $tweet = $('<div class="container"></div>');
+  //   var $image = $('<img class="profile-photo", src="assets/img/mracus.png"></img>');
+  //   var $user = $('<span class="username"></span>');
+  //   var $message = $('<div class="message"></div>');
+  //   var $time = $('<span class="timestamp"></span>');
+  //   var $icon1 = $('<span class="icon"></span>');
+  //   var $icon2 = $('<span class="icon"></span>');
+  //   var $icon3 = $('<span class="icon"></span>');
+  //   var $icon4 = $('<span class="icon"></span>');
+  //   // $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  //   $user.text("@" + tweet.user);
+  //   $message.text(tweet.message);
+  //   $time.append(jQuery.timeago(new Date()));
+  //   $icon1.append('<i class="fas fa-bowling-ball"></i>');
+  //   $icon2.append('<i class="fas fa-bowling-ball"></i>');
+  //   $icon3.append('<i class="fas fa-bowling-ball"></i>');
+  //   $icon4.append('<i class="fas fa-bowling-ball"></i>');
+  //   $tweet.append($image, $user, $message, $time, $icon1, $icon2, $icon3, $icon4);
+  //   $tweet.appendTo($tweets);
+  //   index -= 1;
 
+  //   let username = tweet.user;
+  //   $user.on("click", function() {
+  //     renderUserTweets(username);
+  //   });
 
-  // var renderFeed = function() {
-  //   var $tweets = $('#tweets');
-  //   $tweets.html("");
-
-
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="container"></div>');
-    var $image = $('<img class="profile-photo", src="assets/img/mracus.png"></img>');
-    var $user = $('<span class="username"></span>');
-    var $message = $('<div class="message"></div>');
-    var $time = $('<span class="timestamp"></span>');
-    var $icon1 = $('<span class="icon"></span>');
-    var $icon2 = $('<span class="icon"></span>');
-    var $icon3 = $('<span class="icon"></span>');
-    var $icon4 = $('<span class="icon"></span>');
-    // $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $user.text("@" + tweet.user);
-    $message.text(tweet.message);
-    $time.append(jQuery.timeago(new Date()));
-    $icon1.append('<i class="fas fa-bowling-ball"></i>');
-    $icon2.append('<i class="fas fa-bowling-ball"></i>');
-    $icon3.append('<i class="fas fa-bowling-ball"></i>');
-    $icon4.append('<i class="fas fa-bowling-ball"></i>');
-    $tweet.append($image, $user, $message, $time, $icon1, $icon2, $icon3, $icon4);
-    $tweet.appendTo($tweets);
-    index -= 1;
-
-  }
-
+  // }
 
 
   //   var index = streams.home.length - 1;
